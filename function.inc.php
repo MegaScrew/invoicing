b70f4fad9b16cf2f6cc23106675a83d24429e328
MegaScrew <MegaScrew@mail.ru>
2021-09-03 13:35:09 +0300

    function.inc file added

---
+++ function.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+function weeakToDay(){
+	$week_number = 18;
+	$year = 2011;
+
+	$first_day = date('d/m/Y', $week_number * 7 * 86400 + strtotime('1/1/' . $year) - date('w', strtotime('1/1/' . $year)) * 86400 + 86400);
+	$last_day = date('d/m/Y', ($week_number + 1) * 7 * 86400 + strtotime('1/1/' . $year) - date('w', strtotime('1/1/' . $year)) * 86400);
+}
+?>
