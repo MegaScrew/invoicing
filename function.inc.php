7dada7b6838f12f3edfc7bf58781a7c1b235f3b7
MegaScrew <MegaScrew@mail.ru>
2021-09-08 08:51:17 +0300

    finction readFile added

--- function.inc.php
+++ function.inc.php
@@ -19,4 +19,33 @@ function weekToDay(int $week_number = 1, int $year = 2011){
 
   	return array('first_day' => $first_day, 'last_day' => $last_day, 'days' => $day);
 }
+
+/**
+* Reading file data
+* @param $file - array file from a web form
+* @return array
+*/
+function readFile(array $file){
+	$recordings = [];
+	$i = 0;
+	$uploadfile = basename($file[0]['name']);
+	
+	if (($handle = fopen($uploadfile, "r")) !== FALSE) {
+		while (($row = stream_get_line($handle, 1024 * 1024, "\n")) !== false) {
+			$columns = explode(';', $row);                // разбиваем строку на две части по символу ;
+
+			$recordings[$i][0] = $columns[0];
+			$recordings[$i][1] = $columns[1];
+			$recordings[$i][2] = $columns[2];
+			$recordings[$i][3] = $columns[3];
+			$recordings[$i][4] = $columns[4];
+			$i++;
+		}
+
+		fclose($handle);
+	}
+	unlink($uploadfile);
+
+	return array('recordings' => $recordings);
+}
 ?>
