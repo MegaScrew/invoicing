26fb4dd468d481b21360e56a6f652a7124990705
MegaScrew <MegaScrew@mail.ru>
2021-09-09 17:09:46 +0300

    finished Step 3 and 4

--- check.php
+++ check.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'function.inc.php';
 
+$recordingsNotFound =[];
 switch ($_POST['Step']) {
 	case '1':
 		$yearWeek = explode("-W", $_POST['week']);
@@ -17,46 +18,97 @@ switch ($_POST['Step']) {
 		break;
 	case '2':
 		$recordings = json_decode($_POST['recordings'], true);
-
-		$test = getCompanyList('crm.company.list', $recordings);
-
-		$order = array("\n\t", "\t", "  ", "   ");
-		$replace = '';
+		//$tempRecordings = getCompanyList('crm.company.list', $recordings);
+		$tempRecordings = getBigData('crm.company.list', 54);
 		
 		foreach ($recordings as &$row) {
-			foreach ($test as $rowtest) {
-				if (str_replace($order, $replace, $row[3]) == $rowtest['UF_CRM_1594794891']) {
-					// echo 'here';
+			foreach ($tempRecordings as $rowtest) {
+				if (strcasecmp($row[3], $rowtest['UF_CRM_1594794891']) == 0) {
 					$row[2] = $rowtest['ID'];
-				}else{
-					$row[2] = 0;
 				}				
 			}
 		}
 
-		//echo(json_encode($params, JSON_UNESCAPED_UNICODE));
-		echo '<pre>';
-			echo 'Step 2';
-		echo '</pre>';
-		echo '<pre>';
-			print_r($test);
-		echo '</pre>';
+		checkArr(2, $recordings, $recordingsNotFound);
+
+		if (count($recordingsNotFound) > 0) {
+			$params = array('recordings' => $recordings);
+		}else{
+			$params = array('recordings' => $recordings, 'recordingsNotFound' => $recordingsNotFound);
+		}
+		
+		echo(json_encode($params, JSON_UNESCAPED_UNICODE));
+		
+		// echo '<pre>';
+		// 	echo 'Step 2';
+		// echo '</pre>';
+		// echo '<pre>';
+		// 	print_r($recordings);
+		// echo '</pre>';	
 		break;
 	case '3':
-		echo '<pre>';
-			echo 'Step 3';
-		echo '</pre>';
+		$recordings = json_decode($_POST['recordings'], true);
+		$first_day = json_decode($_POST['first_day'], true);
+		$last_day = json_decode($_POST['last_day'], true);
+
+		updateCompanyFiled($recordings, $first_day, $last_day);
+
+		$params = array('Step3' => 'finish');
+		echo(json_encode($params, JSON_UNESCAPED_UNICODE));
+
+		// echo '<pre>';
+		// 	echo 'Step 3';
+		// echo '</pre>';
+		// echo '<pre>';
+		// 	// print_r($_POST);
+		// 	print_r($first_day);
+		// 	print_r($last_day);
+		// 	// print_r($recordings);
+		// echo '</pre>';
 		break;
 	case '4':
-		echo '<pre>';
-			echo 'Step 4';
-		echo '</pre>';
+		$recordings = json_decode($_POST['recordings'], true);
+		
+		$dealList = getDealList('crm.deal.list', $recordings);
+		
+		foreach ($recordings as &$row) {
+			foreach ($dealList as $rowtest) {
+				if (strcasecmp($row[2], $rowtest['COMPANY_ID']) == 0) {
+					$row[1] = $rowtest['ID'];
+					$row[0] = $rowtest['STAGE_ID'];
+				}				
+			}
+		}
+
+		checkArr(1, $recordings, $recordingsNotFound);
+
+		if (count($recordingsNotFound) > 0) {
+			$params = array('recordings' => $recordings);
+		}else{
+			$params = array('recordings' => $recordings, 'recordingsNotFound' => $recordingsNotFound);
+		}
+		
+		echo(json_encode($params, JSON_UNESCAPED_UNICODE));
+
+		// echo '<pre>';
+		// 	echo 'Step 4';
+		// echo '</pre>';
+		// echo '<pre>';
+		// 	// print_r($_POST);
+		// 	print_r($recordings);
+		// 	print_r($recordingsNotFound);
+		// echo '</pre>';
 		break;
 	case '5':
 		echo '<pre>';
 			echo 'Step 5';
 		echo '</pre>';
 		break;			
+	case '6':
+		echo '<pre>';
+			echo 'Step 6';
+		echo '</pre>';
+		break;	
 	default:
 		echo '<pre>';
 			echo 'default';
