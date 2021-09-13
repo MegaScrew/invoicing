7eb2a791d75074b5ec5baf2e21a5c7d63b6dc374
MegaScrew <MegaScrew@mail.ru>
2021-09-13 14:01:39 +0300

    added checkArr2 function

--- function.inc.php
+++ function.inc.php
@@ -469,4 +469,29 @@ function getAllDeals(string $method = 'crm.deal.list'){
 
     return $totalResult;
 }
+
+/**
+* searches for 1 in an array string and moves the found string to a new array before deleting it from the search array
+* @param $number - number key in row array
+* @param $auto - the array in which we will search
+* @param $manual - the array to which we will transfer the found
+* @return 0
+*/
+function checkArr2(int $number, array &$auto, array &$manual){      // функция для проверки массива
+    $i = 0;
+    $arrSplice = [];
+    foreach ($auto as &$row){             // перебираем массив 
+        if ($row[$number] != 0) {
+            array_push($manual, $row);    
+            array_push($arrSplice, $i);         // записываем индекс документа 
+        }
+        $i++;
+    }
+
+    for ($i=0; $i < count($arrSplice); $i++) {  // в цикле перебираем и удаляем  документы 
+        unset($auto[$arrSplice[$i]]);
+    }
+        
+    $auto = array_values($auto);                // переиндексируем массив что бы индексы были по парядку без дыр на тот случай если захотим идти циклом по индексам 
+}
 ?>
